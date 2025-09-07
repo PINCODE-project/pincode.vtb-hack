591073f939e90a3e9e300e0fc1145e574744a634
Danil <donul.mukhametshin.03@gmail.com>
2025-09-07 22:51:12 +0500

    fix

---
+++ vtb.backend/SqlAnalyzer.Api/Migrations/20250904163547_AddDbConnectionName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SqlAnalyzer.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDbConnectionName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "DbConnections",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "DbConnections");
+        }
+    }
+}
