80b71c5fc57c922b7546837af4f965bce3097999
Maxim Rozhkov <maks.rozhkov.2004@gmail.com>
2025-09-09 16:54:46 +0500

    fix: ci/cd

--- pincode.vtb-hack.fe/next.config.ts
+++ pincode.vtb-hack.fe/next.config.ts
@@ -6,14 +6,6 @@ const nextConfig: NextConfig = {
 	env: {
 		API_BASE_URL: process.env.API_BASE_URL,
 	},
-	async rewrites() {
-		return [
-			{
-				source: "/backend/:path*",
-				destination: `http://localhost:9001/:path*`,
-			},
-		];
-	},
 };
 
 export default nextConfig;
